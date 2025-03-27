8b529020534b11d63292baa6ff8bdd629d1e8447
vseetharama <163523322+vseetharama@users.noreply.github.com>
2025-03-27 16:08:36 +0530

    Create topological_sort.c

---
+++ topological_sort.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+void readgraph(int n,int a[10][10])
+{
+int i,j;
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+scanf("%d",&a[i][j]);
+}}
+void find_indegree(int n,int a[10][10],int indegree[])
+{
+int i,j,sum;
+for(j=0;j<n;j++)
+{
+sum=0;
+for(i=0;i<n;i++)
+sum+=a[i][j];
+indegree[j]=sum;
+}}
+void topological_Sort(int n,int a[10][10])
+{
+int i,u,v,top,t[10],indegree[10],s[10],k=0;
+find_indegree(n,a,indegree);
+top=-1;
+for(i=0;i<n;i++) {
+if(indegree[i]==0) s[++top]=i;
+}
+while(top!=-1)
+{
+u=s[top--];
+t[k++]=u;
+for(v=0;v<n;v++)
+{
+if(a[u][v]==1)
+{
+indegree[v]--;
+if(indegree[v]==0)
+{
+s[++top]=v;
+}}}}
+printf("The topological Sort sequence is:");
+for(i=0;i<n;i++)
+printf("%d\t",t[i]);
+}
+void main()
+{
+int n,a[10][10];
+printf("\nEnter number of values:");
+scanf("%d",&n);
+printf("\nEnter adjacency matrix:");
+readgraph(n,a);
+topological_Sort(n,a);
+}
